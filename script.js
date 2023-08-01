11d6085746dd17274e5b95ffcf0879ed5c5c0c57
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-08-01 21:08:30 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+var object = null;
+var hand1 = "";
+var hand2 = "";
+var index1 = 0;
+var index2 = 0;
+
+function test() {
+    var endpoint = "https://script.google.com/macros/s/AKfycbxW6LFIOauMxA-GUQSQsEpltT-vmPRWmsrjtm4bFq8Tlsvm2kyOYxZko8BX-f4iICC-AQ/exec";
+
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            object = data;
+        });
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    test();
+});
+
+let wait = setInterval(() => {
+    if (object != null) {
+        document.querySelector(".loading").style.transition = 'opacity 1s';
+        document.querySelector(".loading").style.opacity = '0%';
+
+        document.querySelector(".loading").addEventListener('transitionend', function handler() {
+            document.querySelector(".loading").removeEventListener('transitionend', handler);
+            document.querySelector(".loading").style.display = "none";
+        });
+        clearInterval(wait);
+        main();
+    }
+    if (Math.round(Date.now() / 500) % 4 == 0) {
+        document.querySelector("#loadingtitle").innerHTML = "データを取得中です...";
+    } else if (Math.round(Date.now() / 500) % 4 == 1) {
+        document.querySelector("#loadingtitle").innerHTML = "データを取得中です";
+    } else if (Math.round(Date.now() / 500) % 4 == 2) {
+        document.querySelector("#loadingtitle").innerHTML = "データを取得中です.";
+    } else if (Math.round(Date.now() / 500) % 4 == 3) {
+        document.querySelector("#loadingtitle").innerHTML = "データを取得中です..";
+    }
+}, 100);
+
+function main() {
+    object.splice(26, object.length - 25)
+}
+
+function check(){
+    hand1 = document.querySelector("#hand1").value;
+    hand2 = document.querySelector("#hand2").value;
+    index1 = object[0].indexOf(hand1);
+    for (var n = 0; n < 25; n++){
+        if (object[n][0] == hand2){
+            index2 = n;
+            break;
+        }
+    }
+    document.querySelector("#winner").innerHTML = "勝ち：" + object[index1][index2];
+}
